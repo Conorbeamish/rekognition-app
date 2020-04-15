ce8825446b2cf8c52852588e0024b5d1746bd674
conorbeamish <conorbeamish@hotmail.co.uk>
2020-04-15 16:46:20 +0100

    Remove dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ImageAnalysis from "./components/ImageAnalysis"
 
 function App() {
   return (
     <div className="App">
-      
+      <ImageAnalysis/>
     </div>
   );
 }
